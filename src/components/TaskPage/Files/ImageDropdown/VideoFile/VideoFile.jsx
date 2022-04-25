1135e8693e24fb127e572d57db1c2725a9a57003
semyonlinkov <semyonlinkov@gmail.com>
2022-04-25 13:55:13 +0400

    will merge

--- src/components/TaskPage/Files/ImageDropdown/VideoFile/VideoFile.jsx
+++ src/components/TaskPage/Files/ImageDropdown/VideoFile/VideoFile.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
+import styles from './VideoFile.module.scss';
 
 const VideoFile = ({ src }) => {
 	return (
-		<>
+		<div className={styles.wrapper}>
 			<video controls muted>
 				<source src={src} type="video/mp4" />
 			</video>
-		</>
+			<button>Удалить файл</button>
+		</div>
 	);
 };
 
