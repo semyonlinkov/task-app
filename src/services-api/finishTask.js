b463d796f6bcef61d0e7d94a3602c0ce36d68e76
EugenLink <eugenstr232@gmail.com>
2022-04-25 15:40:21 +0300

    merg

---
+++ src/services-api/finishTask.js
@@ -0,0 +1,31 @@
+import {$linkServer} from "../$config";
+
+export const finishTask = (form, id, timeStart, changeHistory) => {
+
+    let formData = new FormData();
+    formData.append('firstTime', timeStart)
+    formData.append('id', id)
+    for (let key in form) {
+        if (key !== 'files') {
+            formData.append([key], form[key]);
+        } else {
+            for (let i = 0; i < form.files.length; i++) {
+                formData.append([form.files[i].name], form.files[i])
+            }
+        }
+    }
+
+    fetch(`${$linkServer}/finishTask.php`, {
+        method: "POST",
+        body: formData
+    })
+        .then(res => res.json())
+        .then(res => {
+            console.log(res)
+            if (res) {
+                alert('Отчёт отправлен!');
+                changeHistory();
+            }
+        })
+        .catch(err => console.log(err));
+}
